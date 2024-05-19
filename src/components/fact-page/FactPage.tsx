359327d58faf95e856e86727a8ca02bb37c87ca3
KateKlu <ktrn0514@gmail.com>
2024-05-19 11:13:03 +0200

    styles

--- src/components/fact-page/FactPage.tsx
+++ src/components/fact-page/FactPage.tsx
@@ -7,11 +7,13 @@ function FactPage() {
    return (
       <div className="container fact-page">
          <h1>Did you know that?</h1>
-         <CatPhoto />
-         <CatFact />
-         <Link to="/cat-facts">
-            <button className="btn">Back</button>
-         </Link>
+         <div className="container-decktop">
+            <CatPhoto />
+            <CatFact />
+            <Link to="/cat-facts">
+               <button className="btn">Back</button>
+            </Link>
+         </div>
       </div>
    );
 }
